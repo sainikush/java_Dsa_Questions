c04dff8ce62fce4ea3968a95958a47d6a9e122da
sainikush <sainikush@users.noreply.github.com>
2026-04-27 23:32:19 +0530

    Linear SEarch

---
+++ src/Lineaar_Search.java
@@ -0,0 +1,10 @@
+public class Lineaar_Search {
+
+    public static void main(String[] args) {
+//    Linear Search (also called sequential search) is a method of finding a target value within a list by checking each element
+//    one by one until the desired element is found or the list ends.
+//   - Works on unsorted arrays/lists.
+//   - Time complexity: O(n) (worst case, you may check all elements).
+//   - Space complexity: O(1) (no extra memory needed).
+    }
+}
