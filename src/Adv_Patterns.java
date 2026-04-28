4f77e6d563c4c2a15e711fffe988efffd80ce873
sainikush <sainikush@users.noreply.github.com>
2026-04-28 07:34:08 +0530

    for loop check

--- src/Adv_Patterns.java
+++ src/Adv_Patterns.java
@@ -95,7 +95,7 @@ public class Adv_Patterns {
         // outer loop
         for (int i = 1; i <= n ; i++) {
             // inner loop
-            for (int j = 1; j <= n-i+1 ; j++) {
+            for (int j = 1; j <= n - i + 1 ; j++) {
                 System.out.print(j);
             }
             System.out.println();
