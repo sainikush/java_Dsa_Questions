3a3762a8d98ca7a09389bca271420abb1abd8cb6
sainikush <sainikush@users.noreply.github.com>
2026-09-09 13:09:39 +0530

    maths

--- src/maths/Mo1.java
+++ src/maths/Mo1.java
@@ -22,11 +22,55 @@ public class Mo1 {
         return count;
     }
 
+    // sum of the digits
+    static int sumofDigits(int num){
+        int sum = 0;
+        while (num != 0){
+            int digit = num%10;
+            sum = sum + digit;
+
+            num = num/10;
+        }
+        return sum;
+    }
+    // Reverse a number
+    static int reverseNum(int num){
+        int revNum = 0;
+
+        while ( num != 0){
+            int digit = num % 10;
+            revNum = revNum*10 + digit;
+            num = num / 10;
+
+        }
+        return revNum;
+    }
+    // Reverse a number
+    static int re2verseNum(int num){
+        int revNum = 0;
+
+        while ( num != 0){
+            int digit = num % 10;
+            revNum = revNum*10 + digit;
+            num = num / 10;
+
+        }
+        return revNum;
+    }
+
+
      static void main(String[] args) {
-        int num = 5241544;
-         printDigits(num);
-         int ans = CountofDigits(num);
-         System.out.println(ans);
+//        int num = 5241544;
+//         printDigits(num);
+//         int ans = CountofDigits(num);
+//         System.out.println("coutn of digit : "+ans);
+//
+//         int sum = sumofDigits(num);
+//         System.out.println(sum);
+
+         int num = 23434;
+         int rwvnum = reverseNum(num);
+         System.out.println(rwvnum);
 
     }
 }
