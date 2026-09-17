06cd9af0e3fc604b10cf2ca9de6e410cace59970
sainikush <sainikush@users.noreply.github.com>
2026-09-17 12:07:07 +0530

    revision

---
+++ src/ThirdTime/First_1.java
@@ -0,0 +1,10 @@
+package ThirdTime;
+
+import java.util.Scanner;
+
+public class First_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
+    }
+}
