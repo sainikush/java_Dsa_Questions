4c3e56f757143954c8fd6d1a5d419f38f9ded2d5
sainikush <sainikush@users.noreply.github.com>
2026-09-19 20:54:15 +0530

    About input and Output

--- src/ThirdTime/First_1.java
+++ src/ThirdTime/First_1.java
@@ -3,8 +3,20 @@ package ThirdTime;
 import java.util.Scanner;
 
 public class First_1 {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println(input.nextInt());
+//    }
+
     public static void main(String[] args) {
+        // Create Scanner instance wrapping Standard Input Stream
         Scanner input = new Scanner(System.in);
-        System.out.println(input.nextInt());
+
+        System.out.print("Please enter your roll number: ");
+        int rollNo = input.nextInt(); // Reads integer token from stream
+
+        System.out.println("Your roll number is " + rollNo);
+
+        input.close(); // Best practice: release underlying resources
     }
 }
